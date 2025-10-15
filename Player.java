59dbe0f50a070a258c4db7cb5096393b6780f102
lara1611 <lschurer16@gmail.com>
2025-10-15 15:59:17 +0000

    add card and discard card

--- Player.java
+++ Player.java
@@ -1,13 +1,33 @@
 public class Player extends Thread{
+    private final int playername;
+    private final int preferredValue;
+    private final List<Card> hand;
 
-public String name;
+    public Player(String playername){
+        this.playername = playername;
+        this.preferredValue = playername;
+        this.hand = new ArrayList<>();
+    }
+    public void run () {
+        System.out.println("player thread!");
 
-public Player(String name){
-    this.name = name;
-}
-public void run () {
-    System.out.println("player thread!");
+    }
+    //player picks up a card
+    public void addCard(Card card) {
+        if (hand.size() < 4) { //each player has 4 cards
+            hand.add(card);
+        }
 
-}
+    }
+    //discard card that is not prefferred value
+    public Card discardCard() {
+        for (Card card : hand) {
+            if (card.getValue() != preferreedValue) {
+                hand.remove(card);
+                return card;
+            }
+        }
+    }
+
 
 }
